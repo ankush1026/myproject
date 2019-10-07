9c3ebe0529b15a24cb449b56e857a23f4bc3e85f
Ankush <ankushdogra111@gmail.com>
2019-10-07 14:18:27 +0530

    This is my second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    New Laravel
                 </div>
 
                 <div class="links">
